58e1ceaaedff9222bbfc0573fa63751b24fef931
bkochman5 <bkochman5@gmail.com>
2025-10-28 19:58:18 +0000

    Complete sum() function task 2

---
+++ 01_First_steps_in_Python/invoking_functions/sum_of_numbers_task2.py
@@ -0,0 +1,7 @@
+#Write a program that takes two integer numbers x and y and then prints the sum of these numbers.
+#Variables x and y are already defined for you. Use the sum() function, please.
+
+x = int(input())
+y = int(input())
+
+print(sum((x, y)))
