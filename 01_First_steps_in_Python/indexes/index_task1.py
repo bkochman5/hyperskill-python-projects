504bc8f48968caf449ff9363a2eca943ab45913a
bkochman5 <bkochman5@gmail.com>
2025-10-29 21:26:05 +0000

    Practicing the indexe

---
+++ 01_First_steps_in_Python/indexes/index_task1.py
@@ -0,0 +1,5 @@
+#We have created a variable alphabet for lowercase alphabets of different languages. Your task is to print the 15th letter of this string.
+
+alphabet = input()
+
+print(alphabet[14])
