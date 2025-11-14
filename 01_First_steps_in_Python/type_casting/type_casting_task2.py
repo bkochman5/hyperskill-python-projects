2c9b414bbe7cd51a64036e1dd1d6fe11354be418
bkochman5 <bkochman5@gmail.com>
2025-11-14 11:42:47 +0000

    Practising type casting

---
+++ 01_First_steps_in_Python/type_casting/type_casting_task2.py
@@ -0,0 +1,13 @@
+# The languages of the world are amazing! Programming languages too, but now we will talk about the human ones.
+# In linguistics, repeating a word or part of it is called reduplication.
+# This morphological phenomenon is found in different languages. Think for a second,
+# and you will definitely come up with a couple of examples. Just to name a few: knock-knock, so-so, bye-bye.
+#
+# We have a full reduplication here since the entire word is repeated.
+# That('s the mechanism we want you to implement. Print a word exactly 2 times. '
+#      'The spelling rules vary across the globe, so do not separate the halves (that will be easy!).)
+
+word = input()
+
+
+print(word * 2)
