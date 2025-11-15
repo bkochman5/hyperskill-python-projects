dba4c7703ee5352430e3175469d58046e717ef78
bkochman5 <bkochman5@gmail.com>
2025-11-15 21:16:34 +0000

    Practising functions

---
+++ 01_First_steps_in_Python/functions/function_task1.py
@@ -0,0 +1,12 @@
+# Let('s take your skill of converting miles to kilometers to the next level!'
+# Define a function that accepts the number of miles and returns this distance in kilometers.)
+#
+# Assume that one mile is approximately equal to 1.609 kilometers.
+#
+# You don't have to handle input, just implement the function.
+
+
+def mi_to_km(miles):
+    km = miles * 1.609
+    return km
+print(mi_to_km(miles=100))
