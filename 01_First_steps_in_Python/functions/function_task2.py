b16db6be94d9a604b7778836639ec3679fa305e0
bkochman5 <bkochman5@gmail.com>
2025-11-17 20:33:48 +0000

    Practising functions task 2

---
+++ 01_First_steps_in_Python/functions/function_task2.py
@@ -0,0 +1,10 @@
+# You are working on a task which requires you to add two numbers together.
+# You have decided to encapsulate this operation in a function for reusable and organized code.
+# You('ve written the necessary lines of code, but in the rush of things, they have been jumbled up.'
+# Your task is to reorder the lines of code to create a valid Python function which takes two arguments,
+# adds them together and then invoke the function and print the result.)
+
+def add_numbers(num1, num2):
+    return num1 + num2
+result = add_numbers(5, 7)
+print(str(result))
