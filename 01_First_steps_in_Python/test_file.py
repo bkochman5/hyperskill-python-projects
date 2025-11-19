80f6150c7f5a284f716bd990da9dea143d975f91
bkochman5 <bkochman5@gmail.com>
2025-11-19 20:37:26 +0000

    Practising find_max function

--- 01_First_steps_in_Python/test_file.py
+++ 01_First_steps_in_Python/test_file.py
@@ -1,6 +1,11 @@
-num1 = int(input())
+def find_max(numbers):
+    current_max = numbers[0]
 
-if num1 % 2 == 0:
-    print(True)
-else:
-    print(False)
+    for num in numbers:
+        if num > current_max:
+            current_max = num
+    return current_max
+
+
+
+print(find_max([1, 2, 10, 99, 1, 20]))
